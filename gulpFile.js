4e8bdec12592bab66c3a66dfed2fe15e88891f01
tarungarg546 <tarungarg546@gmail.com>
2016-06-20 21:12:25 +0530

    travis try

--- gulpFile.js
+++ gulpFile.js
@@ -1,10 +1,10 @@
 "use strict";
 const gulp = require('gulp');
-const notify = require('gulp-notify');
 const jscs = require('gulp-jscs');
 const jshint = require('gulp-jshint');
 const map = require('map-stream');
-const babel=require('gulp-babel');
+const babel = require('gulp-babel');
+const ava = require('gulp-ava');
 const files=[ '*.js',"examples/*.js","source/*.js"];
 gulp.task('transpile',_ => {
     return gulp.src('source/*.js')
@@ -24,11 +24,8 @@ gulp.task('lint',_ => {
                 .pipe(jshint.reporter('jshint-stylish'))
                 .pipe(jshint.reporter("fail"));   
 });
-
-gulp.task('check', ['transpile','jscs', 'lint'],_ => {
-    return gulp.src('/')
-                .pipe(notify({
-                    title: 'Task Builder',
-                    message: 'Successfully built application'
-                }));
+gulp.task('test',['transpile','jscs','lint'],_ => {
+    return  gulp.src('test.js')
+                .pipe(ava());
 });
+gulp.task('default', ['test']);
---
+++ test.js
@@ -0,0 +1,41 @@
+import test from 'ava';
+
+var HE = require('./distribution/');
+
+const hackerEarth = new HE(
+    process.env.HE_KEY, //client secret key
+    ''
+);
+
+const config = {
+      time_limit:1,
+      memory_limit:323244,
+      source:'print "Hello World"',
+      language:"Py"
+  };
+
+test('should be equal to run url',t => {
+  t.is(hackerEarth.runURL,"https://api.hackerearth.com/v3/code/run/");
+});
+
+test('should be equal to compile url',t => {
+  t.is(hackerEarth.compileURL,'https://api.hackerearth.com/v3/code/compile/');
+});
+
+test('should compile  with callback',t => {
+  return hackerEarth.compile(config,(err,op) => {
+    t.is(op.compile_status,"AC");
+  });
+});
+test('should compile  with promise',t => {
+  return hackerEarth.compile(config).then(op=>t.is(op.compile_status,"AC"));
+});
+test('should run  with promise',t => {
+  return hackerEarth.run(config).then(op=>t.is(op.run_status.status,"AC"));
+});
+
+test('should run with callback',t => {
+ return hackerEarth.run(config,(err,op) => {
+    t.is(op.run_status.status,"AC");
+  });
+});
